647199c9e1b55265c6f0fae88aed9b875b8c2cde
phmiranda <pehhagah.1607@gmail.com>
2021-12-18 00:08:48 -0300

    [create] - criação da camada de persistência do serviço.
    [create] - criação da camada de persistência do serviço.

---
+++ src/main/java/br/com/phmiranda/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Author: phmiranda
+ * Project: ecommerce
+ * Task Number: HUXXX
+ * Description: N/A
+ * Date: 17/12/2021
+ */
+
+package br.com.phmiranda.repository;
+
+public interface ProdutoRepository {
+    public void principal();
+}
---
+++ src/main/java/br/com/phmiranda/repository/persistence/ProdutoDao.java
@@ -0,0 +1,19 @@
+/*
+ * Author: phmiranda
+ * Project: ecommerce
+ * Task Number: HUXXX
+ * Description: N/A
+ * Date: 17/12/2021
+ */
+
+package br.com.phmiranda.repository.persistence;
+
+import br.com.phmiranda.repository.ProdutoRepository;
+
+public class ProdutoDao implements ProdutoRepository {
+
+    @Override
+    public void principal() {
+
+    }
+}
