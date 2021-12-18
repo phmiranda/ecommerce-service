fdf800a46ffac4f9a975b992c5a8d3999edd0922
phmiranda <pehhagah.1607@gmail.com>
2021-12-18 00:08:35 -0300

    [create] - criação da camada de domínio do serviço.
    [create] - criação da camada de domínio do serviço.

---
+++ src/main/java/br/com/phmiranda/domain/dto/NotaDto.java
@@ -0,0 +1,12 @@
+/*
+ * Author: phmiranda
+ * Project: ecommerce
+ * Task Number: HUXXX
+ * Description: N/A
+ * Date: 17/12/2021
+ */
+
+package br.com.phmiranda.domain.dto;
+
+public class NotaDto {
+}
